0fcd7f602d281b7d2525829cc8fe700640870043
Unknown <albertov0304@gmail.com>
2018-03-11 15:58:39 -0700

    First time uploading files

---
+++ CastleDefense/app/src/main/java/com/example/alber/castledefense/StartActivity.java
@@ -0,0 +1,29 @@
+package com.example.alber.castledefense;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class StartActivity
+        extends AppCompatActivity {
+
+    private Button mStartButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mStartButton = (Button) findViewById(R.id.start_button);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(StartActivity.this, R.string.loading_toast, Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+    }
+}
