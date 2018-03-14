da7007965dab172cb4039f599cecc97fa2c178dc
Jonathan Kosasih <jonathan.kosasih548@myci.csuci.edu>
2018-03-13 20:29:01 -0700

    Load menu fix
    Fixed forced landscape mode on load menus

--- CastleDefense/app/src/main/java/com/example/alber/castledefense/LoadMenuActivity.java
+++ CastleDefense/app/src/main/java/com/example/alber/castledefense/LoadMenuActivity.java
@@ -63,12 +63,19 @@ public class LoadMenuActivity extends AppCompatActivity {
     }
 
     private void setToFullScreen(){
-        ViewGroup mainMenu = (ViewGroup) findViewById(R.id.activity_main);
+        ViewGroup mainMenu = (ViewGroup) findViewById(R.id.activity_load_menu);
         mainMenu.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    // After closing the app and reopening it, sets fullscreen mode
+    @Override
+    protected void onResume(){
+        super.onResume();
+        setToFullScreen();
     }
 }
